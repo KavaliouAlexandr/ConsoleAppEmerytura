aaee8f71b97029768eba3f896e9121ce0fae3e88
Ковалев Александр <alexandr.kavaliou@microsoft.wsei.edu.pl>
2021-03-15 23:17:52 +0100

    konf

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleAppEmerytura
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");fefefee
         }
     }
 }
